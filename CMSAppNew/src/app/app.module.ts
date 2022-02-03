bed1629e73cb6a4ec6cd04f46e01b3fa139f9d45
Sneha K Salu <sneha.salu@experionglobal.com>
2022-02-03 15:24:41 +0530

    by sneha

--- CMSAppNew/src/app/app.module.ts
+++ CMSAppNew/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< HEAD
 import { PatientsComponent } from './patients/patients.component';
 import { PatientComponent } from './patients/patient/patient.component';
 import { PatientListComponent } from './patients/patient-list/patient-list.component';
@@ -10,21 +11,44 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { Ng2SearchPipeModule } from 'ng2-search-filter';
 import { FormsModule } from '@angular/forms';
+=======
+import { StaffsComponent } from './staffs/staffs.component';
+import { StaffComponent } from './staffs/staff/staff.component';
+import { StaffListComponent } from './staffs/staff-list/staff-list.component';
+import { FormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { Ng2SearchPipeModule } from 'ng2-search-filter';
+import { HttpClientModule } from 'ngx-http-client';
+>>>>>>> 08e16812546e3a8c3bcfd397be8114b8a8beb305
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< HEAD
     PatientsComponent,
     PatientComponent,
     PatientListComponent
+=======
+    StaffsComponent,
+    StaffComponent,
+    StaffListComponent
+   
+>>>>>>> 08e16812546e3a8c3bcfd397be8114b8a8beb305
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+<<<<<<< HEAD
     HttpClientModule,
     NgxPaginationModule,
     Ng2SearchPipeModule,
     FormsModule
+=======
+    FormsModule,
+    NgxPaginationModule,
+    Ng2SearchPipeModule,
+    HttpClientModule
+>>>>>>> 08e16812546e3a8c3bcfd397be8114b8a8beb305
   ],
   providers: [PatientService],
   bootstrap: [AppComponent]
---
+++ CMSAppNew/src/app/shared/staff.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Staff } from './staff';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StaffService {
+
+  staffs : Staff[];
+  formData : Staff = new Staff();
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllPosts():Observable<any>{
+    return this.httpClient.get(environment.apiUrl+'/api/post');
+  }
+
+
+  bindListPosts(){
+    this.httpClient.get(environment.apiUrl+'api/staff/GetStaffsAll')
+    .toPromise().then(
+      response =>{
+        console.log("From Services");
+        console.log(response);
+        this.staffs = response as Staff[]
+      }
+    );
+  }
+
+  deletePost(id : number){
+    return this.httpClient.delete(environment.apiUrl+'api/staff/'+id);
+  }
+
+  getDoctor(){
+    this.httpClient.get(environment.apiUrl+'/api/staff/getStaffsAll')
+  }
+
+}
